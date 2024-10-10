aba9498428d4e7202c6ee5c187d85c194ed49fe3
Sall <naabilasalsabiilaa@gmail.com>
2024-10-10 20:52:58 +0700

    Modul Kuantum Azimut

--- Modul kuantum utama.py
+++
@@ -1,6 +0,0 @@
-modulKulit = __import__('Modul kulit')
-
-def kuantumUtama(nomor_atom):
-    konfigurasi = modulKulit.konfigurasi_elektron(nomor_atom)
-
-    return konfigurasi[len(konfigurasi) - 3]
